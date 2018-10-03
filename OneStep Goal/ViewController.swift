8093febdf0cecf3d21e4d6df01331a7f7d88eb4a
Bernard Huff <bernard.huff008@gmail.com>
2018-10-03 17:59:17 -0500

    Added code thet clears the screen after user enters new to do list item

--- OneStep Goal/ViewController.swift
+++ OneStep Goal/ViewController.swift
@@ -5,18 +5,66 @@
 //  Created by Bernard Huff on 10/2/18.
 //  Copyright © 2018 Flyhightech.LLC. All rights reserved.
 //
-//  Test 2
 
 import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var textField: NSTextField!
+    
+    @IBOutlet weak var importantCheckbox: NSButton!
+    
+    var toDoItems : [ToDoItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        getToDoListItem()
+        
+    }
+    
+//  Below is the code that fetches the to do list items
+    
+    func getToDoListItem() {
+        
+        if let context = (NSApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+//  Below is a do, try, catch, statement
+            
+            do {
+                toDoItems = try context.fetch(ToDoItem.fetchRequest())
+            } catch {}
+        }
+        
     }
 
+    @IBAction func addClicked(_ sender: Any) {
+        
+        if textField.stringValue != "" {
+            
+            if let context = (NSApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                
+                let toDoItem = ToDoItem(context: context)
+                
+                toDoItem.name = textField.stringValue
+                
+                if importantCheckbox.state.rawValue == 0 {
+                    toDoItem.important = false
+                } else {
+                    toDoItem.important = true
+                }
+                
+                (NSApplication.shared.delegate as? AppDelegate)?.saveAction(nil)
+                
+//  Below is the code that clears out the screen after user enters new item.
+                
+                textField.stringValue = ""
+                importantCheckbox.state = NSControl.StateValue(rawValue: 0)
+                getToDoListItem()
+                
+            }
+        }
+    }
     
 
 
@@ -27,5 +75,5 @@ class ViewController: NSViewController {
     
     
     
-} //End of the view controller class
+} //    End of the view controller class
 
